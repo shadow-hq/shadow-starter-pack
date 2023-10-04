dd8f23589681807445f5a840e186be4e73bcf461
emilyhsia <emily.j.hsia@gmail.com>
2023-10-04 12:53:00 -0400

    update NonfungiblePositionManager uniswap interfaces

--- contracts/src/NonfungiblePositionManager/@uniswap/v3-core/contracts/interfaces/pool/IUniswapV3PoolActions.sol
+++ contracts/src/NonfungiblePositionManager/@uniswap/v3-core/contracts/interfaces/pool/IUniswapV3PoolActions.sol
@@ -28,6 +28,25 @@ interface IUniswapV3PoolActions {
         bytes calldata data
     ) external returns (uint256 amount0, uint256 amount1);
 
+    /// @notice Adds liquidity for the given recipient/tickLower/tickUpper position, when coming from nonfungible position manager
+    /// @dev This function includes positionId
+    /// @param owner The address that calls NonfungiblePositionManager
+    /// @param recipient NonfungiblePositionManager
+    /// @param tickLower The lower tick of the position in which to add liquidity
+    /// @param tickUpper The upper tick of the position in which to add liquidity
+    /// @param amount The amount of liquidity to mint
+    /// @param data Any data that should be passed through to the callback
+    /// @return amount0 The amount of token0 that was paid to mint the given amount of liquidity. Matches the value in the callback
+    /// @return amount1 The amount of token1 that was paid to mint the given amount of liquidity. Matches the value in the callback
+    function nFPMint(
+        address owner, // address that calls NonfungiblePositionManager
+        address recipient, // usually NonfungiblePositionManager
+        int24 tickLower,
+        int24 tickUpper,
+        uint128 amount,
+        bytes calldata data
+    ) external returns (uint256 amount0, uint256 amount1);
+
     /// @notice Collects tokens owed to a position
     /// @dev Does not recompute fees earned, which must be done either via mint or burn of any amount of liquidity.
     /// Collect must be called by the position owner. To withdraw only token0 or only token1, amount0Requested or
@@ -48,6 +67,28 @@ interface IUniswapV3PoolActions {
         uint128 amount1Requested
     ) external returns (uint128 amount0, uint128 amount1);
 
+    /// @notice Collects tokens owed to a position
+    /// @dev Does not recompute fees earned, which must be done either via mint or burn of any amount of liquidity.
+    /// Collect must be called by the position owner. To withdraw only token0 or only token1, amount0Requested or
+    /// amount1Requested may be set to zero. To withdraw all tokens owed, caller may pass any value greater than the
+    /// actual tokens owed, e.g. type(uint128).max. Tokens owed may be from accumulated swap fees or burned liquidity.
+    /// @param owner The address that calls the NonfungiblePositionManager collect function
+    /// @param recipient The address which should receive the fees collected
+    /// @param tickLower The lower tick of the position for which to collect fees
+    /// @param tickUpper The upper tick of the position for which to collect fees
+    /// @param amount0Requested How much token0 should be withdrawn from the fees owed
+    /// @param amount1Requested How much token1 should be withdrawn from the fees owed
+    /// @return amount0 The amount of fees collected in token0
+    /// @return amount1 The amount of fees collected in token1
+    function nFPCollect(
+        address owner, // address that calls NonfungiblePositionManager
+        address recipient, // usually NonfungiblePositionManager
+        int24 tickLower,
+        int24 tickUpper,
+        uint128 amount0Requested,
+        uint128 amount1Requested
+    ) external returns (uint128 amount0, uint128 amount1);    
+
     /// @notice Burn liquidity from the sender and account tokens owed for the liquidity to the position
     /// @dev Can be used to trigger a recalculation of fees owed to a position by calling with an amount of 0
     /// @dev Fees must be collected separately via a call to #collect
@@ -57,6 +98,21 @@ interface IUniswapV3PoolActions {
     /// @return amount0 The amount of token0 sent to the recipient
     /// @return amount1 The amount of token1 sent to the recipient
     function burn(
+        int24 tickLower,
+        int24 tickUpper,
+        uint128 amount
+    ) external returns (uint256 amount0, uint256 amount1);
+
+    /// @notice Burns liquidity for the given recipient/tickLower/tickUpper position, when coming from nonfungible position manager
+    /// @dev This function includes positionId
+    /// @dev Fees must be collected separately via a call to #collect
+    /// @param tickLower The lower tick of the position for which to burn liquidity
+    /// @param tickUpper The upper tick of the position for which to burn liquidity
+    /// @param amount How much liquidity to burn
+    /// @return amount0 The amount of token0 sent to the recipient
+    /// @return amount1 The amount of token1 sent to the recipient
+    function nFPBurn(
+        address owner, // address that calls NonfungiblePositionManager
         int24 tickLower,
         int24 tickUpper,
         uint128 amount
